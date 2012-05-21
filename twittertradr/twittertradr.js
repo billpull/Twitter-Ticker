dbf11ba739896a5c738fc45a1515369dea1a3d2e
Bill Pullen <bill.pull@gmail.com>
2012-05-21 01:48:34 -0400

    updated packed extension

--- twittertradr/twittertradr.js
+++ twittertradr/twittertradr.js
@@ -27,8 +27,7 @@ var neu_tmpl = "\
 	</a>";
 
 $tt(function(){
-
-    
+  replaceStockSymbols();
 });
 
 function replaceStockSymbols(){
@@ -148,4 +147,27 @@ function queryYahooFinance(symbols, callback){
     success:callback
   });
 }
+
+  jQuery.fn.contentChange = function(callback){
+    var elms = jQuery(this);
+    elms.each(
+      function(i){
+        var elm = jQuery(this);
+        elm.data("lastContents", elm.html());
+        window.watchContentChange = window.watchContentChange ? window.watchContentChange : [];
+        window.watchContentChange.push({"element": elm, "callback": callback});
+      }
+    )
+    return elms;
+  }
+  setInterval(function(){
+    if(window.watchContentChange){
+      for( i in window.watchContentChange){
+        if(window.watchContentChange[i].element.data("lastContents") != window.watchContentChange[i].element.html()){
+          window.watchContentChange[i].callback.apply(window.watchContentChange[i].element);
+          window.watchContentChange[i].element.data("lastContents", window.watchContentChange[i].element.html())
+        };
+      }
+    }
+  },500);
 
